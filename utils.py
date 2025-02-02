f5eb16804df9182bc139acf39a4b52438258a3e1
JAYASIRI S.P.A.S. <ict21860@fot.sjp.ac.lk>
2025-02-02 16:16:17 +0530

    Add temp password list

--- utils.py
+++ utils.py
@@ -10,4 +10,15 @@ text = figlet.renderText("AirCrackX")
 # Print colored text using Rich
 console.print(f"[cyan]{text}[/cyan]")
 
+password_list = []
 
+def load_password_list():
+    file = open("password.txt","r")
+    l1 = file.readlines()
+    for i in l1:
+        new_password,emptySpace,extra = i.split(" ")
+        password_list.append(new_password)
+
+load_password_list()
+
+print(password_list)
